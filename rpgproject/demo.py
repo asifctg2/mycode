c7f9a9f44431544a6fbc5305bb267a2652c2ad5f
Asif Haider <student@bchd.03d4a2e5-f75a-4f2f-84b4-6be5ac0bb21d>
2022-07-05 18:00:09 +0000

    reviewing how to manipulate JSON data with python

---
+++ rpgproject/demo.py
@@ -0,0 +1,23 @@
+classinfo = {
+    "all": [
+        {
+            "name": "Zach",
+            "skill level": "wondrous",
+            "spirit animal": "Alpaca",
+            "super power": "Structure Weakening",
+        },
+    print (all)
+
+
+
+
+#quiz = {"what is your name" : "asif"}
+
+#val = input("enter your answer:")
+#print(val)
+
+#if quiz.get("asif")!=None:
+#    print("the key is present.\n")
+
+#else:
+#    print ("the key does not exist.")
